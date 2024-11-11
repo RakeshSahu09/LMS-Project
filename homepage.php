aaefdb7481068add6c9bd182ff84d0eb0564132f
RakeshSahu09 <rakeshsahu5050@gmail.com>
2024-11-11 14:22:16 +0530

    Initial commit

---
+++ homepage.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+include("connect.php");
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div style="text-align:center; padding:80px; margin:80px;">
+      <p  style="font-size:50px; font-weight:bold; margin:80px;">
+       Hello  <?php 
+       if(isset($_SESSION['email'])){
+        $email=$_SESSION['email'];
+        $query=mysqli_query($conn, "SELECT users.* FROM `users` WHERE users.email='$email'");
+        while($row=mysqli_fetch_array($query)){
+            echo $row['firstName'].' '.$row['lastName'];
+        }
+       }
+       ?>
+       :)
+      </p>
+      <a href="lms_portal\index.html" class= "Getstarted" >Get Started</a>
+    </div>
+</body>
+</html>
